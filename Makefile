cf4ebb7465b0de5238bb21f5260e8121a9040464
xelastone <126184753+lalolanda11@users.noreply.github.com>
2023-08-06 13:11:34 -0600

    Update and rename Makefile to instalacion.sh

--- Makefile
+++
@@ -1,9 +0,0 @@
-PROGRAM_NAME=xela
-PROGRAM_DIR=/data/data/com.termux/files/usr/bin
-
-install:
-  install -Dm755  xela.sh $(PROGRAM_DIR)/$(PROGRAM_NAME)
-
-uninstall:
-  rm -rf $(PROGRAM_DIR)/$(PROGRAM_NAME)
-
---
+++ instalacion.sh
@@ -0,0 +1,32 @@
+#!bin/env/ bash
+PROGRAM_NAME=xela
+PROGRAM_DIR=/data/data/com.termux/files/usr/bin
+instalar(){
+install -Dm755  xela.sh $(PROGRAM_DIR)/$(PROGRAM_NAME)
+}
+desinstalar(){
+rm -rf $(PROGRAM_DIR)/$(PROGRAM_NAME)
+}
+
+echo -e "Instalar programa \n [si] \n [no] "
+read opcion
+case $opcion in 
+  si)
+    instalar
+    ;;
+  no)
+    echo -n "¿Desea desinstalar el programa ?"
+    read opcion
+    if [ $opcion == "si" ];then
+        desinstalar
+        echo "adios"
+    else
+      echo "No se desinstalo el programa"
+      fi
+      ;;
+    **)
+        echo "No se encontro un parametro valido"
+        clear
+        ;;
+
+esac
